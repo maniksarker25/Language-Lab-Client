4080d3f64462ee1a36777dd0c5fe4f3be39c3ad3
Manik <tonysins43@gmail.com>
2023-06-06 23:48:06 +0600

    project setup install react router dom , tailwind, daisyui

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>this is language lab</h1>
+        <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+    </>
+  )
+}
+
+export default App
