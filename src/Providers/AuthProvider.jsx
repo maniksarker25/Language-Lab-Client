14b2d5749f808b773e4fb90dfbd9b884b2c4218d
Manik <tonysins43@gmail.com>
2023-06-07 15:38:49 +0600

    update user profile

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -7,6 +7,7 @@ import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
+  signOut,
   updateProfile,
 } from "firebase/auth";
 import app from "../Firebase/firebase.config";
@@ -15,19 +16,27 @@ const googleProvider = new GoogleAuthProvider();
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // handle sign up
   const signUp = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   // handle login
   const logIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   // handle google sign in
   const googleSignIn = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
+  //logOut 
+  const logOut = () =>{
+     return signOut(auth)
+  }
   // update user profile
   const updateUserProfile = (name, photoURL) => {
     return updateProfile(auth.currentUser, {
@@ -35,23 +44,30 @@ const AuthProvider = ({ children }) => {
       photoURL: photoURL,
     });
   };
-  
-  // find current user 
-  useEffect(()=>{
-    const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+
+
+  // find current user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      
-    })
-  },[]) 
+      // console.log(currentUser)
+      setLoading(false);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
 
   const authInfo = {
     user,
     signUp,
     logIn,
     googleSignIn,
-    updateUserProfile
-    
-    
+    updateUserProfile,
+    logOut,
+    loading,
+    setLoading
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
