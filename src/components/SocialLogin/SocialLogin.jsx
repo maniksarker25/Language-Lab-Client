91223764e8dc9c25fb66fab8b2ef7de034358763
Manik <tonysins43@gmail.com>
2023-06-08 09:22:39 +0600

    add user in database when login with google by using axios

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,5 +1,40 @@
 import { FaGoogle } from "react-icons/fa";
+import useAuth from "../../Hooks/UseAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 const SocialLogin = () => {
+  const { googleSignIn, setLoading } = useAuth();
+  // const navigate = useNavigate();
+  // const location = useLocation();
+  // const from = location.state?.from?.pathname || "/";
+  // handle google Sign in
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        axios
+          .post("http://localhost:5000/users", {
+            name: loggedInUser.displayName,
+            email: loggedInUser.email,
+            photoUrl: loggedInUser.photoURL,
+            role: "student",
+          })
+          .then((data) => {
+            if (data.data.insertedId) {
+              setLoading(false);
+            }
+          })
+          .catch(error=>{
+            console.log(error.message)
+          })
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="text-center">
       <div className="flex items-center justify-center gap-2">
@@ -7,7 +42,10 @@ const SocialLogin = () => {
         <span>or</span>
         <hr className=" w-1/2 lg:w-1/3 " />
       </div>
-      <button className="flex primary-btn py-2 rounded-md text-white font-semibold w-full my-4 items-center justify-center gap-2 hover:bg-[]">
+      <button
+        onClick={handleGoogleSignIn}
+        className="flex primary-btn py-2 rounded-md text-white font-semibold w-full my-4 items-center justify-center gap-2 hover:bg-[]"
+      >
         <FaGoogle /> Continue With Google
       </button>
     </div>
